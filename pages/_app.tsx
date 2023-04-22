ed45203613101989c13182c5e9fdb1404a891bb8
thanhtungtav404 <nguyenthanhtung74119@gmail.com>
2023-04-22 11:23:36 +0700

    tungnt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
 import '../styles/index.css'
+const roboto = Roboto({
+  weight: '500',
+  subsets: ['latin'],
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+   <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
